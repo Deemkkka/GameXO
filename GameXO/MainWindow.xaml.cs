1c53e008fedd1e9abc28ce7d89337e79566ce1c4
Deem <Deem@DEEM>
2022-05-30 16:23:51 +0300

    Добавьте файлы проекта.

---
+++ GameXO/MainWindow.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Windows;
+
+
+namespace GameXO
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        #region Private Members
+
+        ///<summary>
+        ///текущие результаты ячеек в активной игре
+        /// </summary>
+        private MarkType[] mResults;
+
+        /// <summary>
+        /// True - если ходит игрок 1 (Х) или игрок 2 (О)
+        /// </summary>
+        private bool mPlayerMove;
+        
+        /// <summary>
+        /// True - если игра окончена
+        /// </summary>
+        private bool mGameEnded;
+
+
+        #endregion
+
+        #region Constructor
+
+
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            Game();
+        }
+
+
+
+        #endregion
+        
+        
+        private void Game()
+        {
+            //массив свободных клеток
+            mResults = new MarkType[9];
+        }
+
+
+    }
+}
---
+++ GameXO/MarkType.cs
@@ -0,0 +1,22 @@
+﻿
+namespace GameXO
+{
+    /// <summary>
+    /// тип значения клетки игры
+    /// </summary>
+    public enum MarkType
+    {
+        /// <summary>
+        /// не нажатая клетка
+        /// </summary>
+        Free,
+        /// <summary>
+        /// Клетка "О"
+        /// </summary>
+        Nought,
+        /// <summary>
+        /// Клетка "X"
+        /// </summary>
+        Cross
+    }
+}
