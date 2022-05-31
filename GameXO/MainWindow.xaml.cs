2a61eedcd4de8b4bd16620c80462b2ec06d06153
Deem <Deem@DEEM>
2022-05-31 07:49:02 +0300

    создается поле, происходит нажатие по клеткам игрока 1 "X"

--- GameXO/MainWindow.xaml.cs
+++ GameXO/MainWindow.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using System;
+using System.Linq;
 using System.Windows;
-
+using System.Windows.Controls;
+using System.Windows.Media;
 
 namespace GameXO
 {
@@ -20,7 +22,7 @@ namespace GameXO
         /// <summary>
         /// True - если ходит игрок 1 (Х) или игрок 2 (О)
         /// </summary>
-        private bool mPlayerMove;
+        private bool mPlayer1Move;
         
         /// <summary>
         /// True - если игра окончена
@@ -46,12 +48,81 @@ namespace GameXO
         #endregion
         
         
+        /// <summary>
+        /// Стартовать новую игру и очистить значения обратно перед стартом
+        /// </summary>
         private void Game()
         {
             //массив свободных клеток
             mResults = new MarkType[9];
+
+            for (int i = 0; i < mResults.Length; i++)
+            {
+                mResults[i] = MarkType.Free;
+            }
+
+            //игрок1 начинает игру
+            mPlayer1Move = true;
+
+            // взаимодействовать с каждой кнопкой
+            GridCells.Children.Cast<Button>().ToList().ForEach(button =>
+            {
+                // изменить цвет фона, цвет контента и очистить контент
+                button.Content = string.Empty;
+                button.Background = Brushes.White;
+                button.Foreground = Brushes.Blue;
+            });
+
+            //убедиться, что игра не закончена
+            mGameEnded = false;
         }
 
 
+        /// <summary>
+        /// обработка события клика
+        /// </summary> 
+        /// <param name="sender">кнопка, которая была нажата</param>
+        /// <param name="e">событие клика</param>
+        private void btn_Click(object sender, RoutedEventArgs e)
+        {
+            // начать новую игру при клике после окончания
+            if (mGameEnded)
+            {
+                Game();
+                return;
+            }
+
+            // передать отправителю кнопки
+            var button = (Button)sender;
+            
+            //найти позиции кнопки в массиве
+            var column = Grid.GetColumn(button);
+            var row = Grid.GetRow(button);
+
+            var index = column + (row * 3);
+
+            //ничего не делаем, если уже есть значение
+            if (mResults[index] != MarkType.Free)
+            {
+                return;
+            }
+
+            //установить значение ячейки, который выполняет игрок
+
+            mResults[index] = mPlayer1Move ? MarkType.Cross : MarkType.Nought;
+
+            //if (mPlayer1Move)
+            //{
+            //    mResults[index] = MarkType.Cross;
+            //}
+            //else
+            //{
+            //    mResults[index] = MarkType.Nought; 
+            //}
+
+            //присваиваем значение 
+            button.Content = mPlayer1Move ? "X" : "O";
+            
+        }
     }
 }
